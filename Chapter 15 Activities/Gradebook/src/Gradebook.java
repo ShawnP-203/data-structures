4939a75050a65ec6e1c7f9db58e3f9b734a26f04
ShawnP-203 <sjplackiyil@stu.naperville203.org>
2024-09-12 14:07:48 -0500

    pasting Priya's code for Gradebook.java

--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,16 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-. . .
+import java.util.Set;
+
 /**
+ * Shawn and Priya
+ * 9-12-2024
  * A program to add, remove, modify or print
  * student names and grades.
 */
+
 public class Gradebook
 {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
 
-        . . .
+        Map<String, String> studentGrade = new HashMap<>();
 
         boolean done = false;
         while(!done)
@@ -20,23 +27,50 @@ public class Gradebook
             if (input.equals("Q"))
             {
                 done = true;
-            } else if (input.equals("A"))
+            } else if (input.equalsIgnoreCase("A"))
             {
-                . . .
+                System.out.println("What is the name of the student? ");
+                String student = in.next();
+                System.out.println("What is the grade of the student? ");
+                String grade = in.next();
 
-            } else if (input.equals("R"))
+                if(!studentGrade.keySet().contains(student))
+                    studentGrade.put(student, grade);
+                else
+                    System.out.println("That student is already in the gradebook.");
+
+            } else if (input.equalsIgnoreCase("R"))
             {
-                . . .
-            } else if (input.equals("M"))
+                System.out.println("What is the name of the student? ");
+                String student = in.next();
+
+                studentGrade.remove(student);
+                
+            } else if (input.equalsIgnoreCase("M"))
             {
-                . . .
+                System.out.println("What is the name of the student? ");
+                String student = in.next();
+                System.out.println("What is the grade of the student? ");
+                String grade = in.next();
+
+                if(studentGrade.keySet().contains(student))
+                    studentGrade.put(student, grade);
+                else
+                    System.out.println("That student is not in the gradebook.");
+
             } else if (input.equalsIgnoreCase("P"))
             {
-                . . .
+                Set<String> keys = studentGrade.keySet();
+                ArrayList<String> keyList = new ArrayList<>(keys);
+                keyList.sort(null);
+                for (String key: keyList)
+                    System.out.println(key + ": " + studentGrade.get(key));
             } else
             {
                 done = true;
             }
         }
+
+        in.close();
     }
 }
