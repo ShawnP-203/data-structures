a8c637e23d77ebd732fe84aa7905b729106af9ef
ShawnP-203 <sjplackiyil@stu.naperville203.org>
2024-11-01 14:14:19 -0500

    partially writing notes for BinarySearchTree

--- Chapter 17 Class Notes/src/BinarySearchTree.java
+++ Chapter 17 Class Notes/src/BinarySearchTree.java
@@ -12,16 +12,24 @@ public class BinarySearchTree
     */
     public BinarySearchTree()
     {   
-        
+        root = null;
     }
     
     /**
         Inserts a new node into the tree.
         @param obj the object to insert
     */
-    public void add(Comparable obj) 
+    public void add(Comparable<Object> obj) 
     {   
-        
+        Node newNode = new Node();
+        newNode.data = obj;
+        newNode.left = null;
+        newNode.right = null;
+
+        if(root == null)
+            root = newNode;
+        else
+            root.addNode(newNode);
     }
 
     /**
@@ -29,8 +37,19 @@ public class BinarySearchTree
         @param obj the object to find
         @return true if the object is contained in the tree
     */
-    public boolean find(Comparable obj)
+    public boolean find(Comparable<Object> obj)
     {
+        Node target = root;
+        while(target != null)
+        {
+            int diff = obj.compareTo(target.data);
+            if(diff == 0)
+                return true;
+            else if(diff < 0)
+                target = target.left;
+            else
+                target = target.right;
+        }
         return false;
     }
     
@@ -39,9 +58,34 @@ public class BinarySearchTree
         if the object is not contained in the tree.
         @param obj the object to remove
     */
-    public void remove(Comparable obj)
+    public void remove(Comparable<Object> obj)
     {
-        
+        if(!find(obj)) return;
+
+        Node target = root;
+        Node parent = null;
+        boolean opComplete = false;
+
+        while(!opComplete)
+        {
+            int diff = obj.compareTo(target.data);
+            if(diff == 0)
+            {
+                Node newChild;
+                if(parent == null)
+                    root = newChild;
+                if(target.left == null && target.right == null)
+                    target = null;
+                else if(target.left == null ^ target.right == null)
+            } else
+            {
+                parent = target;
+                if(diff < 0)
+                    target = target.left;
+                else
+                    target = target.right;
+            }
+        }
     }
     
     /**
@@ -67,7 +111,9 @@ public class BinarySearchTree
     */
     static class Node
     {   
-        
+        public Comparable<Object> data;
+        public Node left;
+        public Node right;
 
         /**
             Inserts a new node as a descendant of this node.
@@ -75,7 +121,21 @@ public class BinarySearchTree
         */
         public void addNode(Node newNode)
         {   
-            
+            int diff = newNode.data.compareTo(data);
+
+            if(diff < 0)
+            {
+                if(left == null)
+                    left = newNode;
+                else
+                    left.addNode(newNode);
+            } else if(diff > 0)
+            {
+                if(right == null)
+                    right = newNode;
+                else
+                    right.addNode(newNode);
+            }
         }
     }
 }
